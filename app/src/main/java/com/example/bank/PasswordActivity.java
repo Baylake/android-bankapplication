40ae052037160d70c9b8f127c1c903a4f65ebdad
Baylake <114943348+Baylake@users.noreply.github.com>
2023-03-01 17:12:49 +0300

    Creates second activity

---
+++ app/src/main/java/com/example/bank/PasswordActivity.java
@@ -0,0 +1,64 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class PasswordActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(OnClickListener);
+        button2.setOnClickListener(OnClickListener);
+
+    }
+
+    /**
+     * Listens two buttons
+     **/
+    View.OnClickListener OnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView answer = findViewById(R.id.textView4);
+            TextView suka = findViewById(R.id.textView);
+            EditText Number1 = findViewById(R.id.editTextNumber2);
+            EditText Number2 = findViewById(R.id.editTextNumber4);
+            switch (v.getId()) {
+                case R.id.button:
+                    Integer int1;
+                    int1 = Integer.parseInt(Number1.getText().toString());
+                    Integer int2;
+                    int2 = Integer.parseInt(Number2.getText().toString());
+                    Integer ans = int1 + int2;
+                    answer.setText(ans.toString());
+                    suka.setText("Sanya Huy Sosi!!!!!!!");
+                    break;
+
+                case R.id.button2:
+
+                    startMainActivity(v);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Starts main activity
+     **/
+    public void startMainActivity(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
